65c35a1620276c841d2314fd3b463b75044e8ecb
ata324 <mavican324@gmail.com>
2024-08-16 18:05:51 +0200

    update

---
+++ src/app/deneme/page.jsx
@@ -0,0 +1,12 @@
+import Uretim from '@/components/uretimPlanlama/uretim'
+import React from 'react'
+
+const DenemePage = () => {
+  return (
+    <div>
+        <Uretim/>
+    </div>
+  )
+}
+
+export default DenemePage
