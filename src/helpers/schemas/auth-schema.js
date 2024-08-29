67e1f53099dff9ccef11ed5fad6e5fc448165a6b
Mikail Ozkan <ozkan.mkl.ozkanQgmail.com>
2024-08-29 10:45:47 +0200

    Password can not be empty

--- src/helpers/schemas/auth-schema.js
+++ src/helpers/schemas/auth-schema.js
@@ -3,5 +3,5 @@ import * as Yup from "yup";
 
 export const AuthSchema = Yup.object({
 	username: Yup.string().required("Required"),
-	password: Yup.string().required("Required"),
+	password: Yup.string().required("Şifre alanı boş bırakılamaz"),
 });
