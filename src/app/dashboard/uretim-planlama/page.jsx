9ba6115bccc464ff1a9329f790646a8041508459
Mikail Ozkan <ozkan.mkl.ozkanQgmail.com>
2024-09-02 00:04:57 +0200

    üretim planlama sayfası düzenlendi

--- src/app/dashboard/uretim-planlama/page.jsx
+++ src/app/dashboard/uretim-planlama/page.jsx
@@ -1,12 +1,30 @@
-import UretimPage from '@/components/dashboard/uretim-planlama/uretim-planlama';
+import UretimPlanlama from '@/components/dashboard/uretim-planlama/uretim-planlama';
+import {getOrders} from '@/services/uretimplanlama-service';
 import React from 'react';
 
-const UretimPlanlama = () => {
+const UretimPlanlamaPage = async ({ searchParams }) => {
+    const currentPage = parseInt(searchParams.currentPage, 10) || 0;
+    const sortBy = searchParams.sortBy || 'orderDate';
+    const sortOrder = searchParams.sortOrder || 'desc';
+
+    const res = await getOrders(currentPage, 10, sortBy, sortOrder);
+    const data = await res.json();
+
+    if (res.status !== 200) {
+        return <div>Error: {res.statusText}</div>;
+    }
+
+
     return (
         <>
-            <UretimPage />
+            <UretimPlanlama 
+                data={data}
+                currentPage={currentPage}
+                sortBy={sortBy}
+                sortOrder={sortOrder}
+             />
         </>
     );
 };
 
-export default UretimPlanlama;
+export default UretimPlanlamaPage;
