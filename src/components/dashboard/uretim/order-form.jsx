e49da7d0ffbc751924d45513d0cccb9d4574c243
Mikail Ozkan <ozkan.mkl.ozkanQgmail.com>
2024-09-09 20:51:13 +0200

    Changaes to be committed:     modified:   src/components/dashboard/uretim/order-form.jsx

--- src/components/dashboard/uretim/order-form.jsx
+++ src/components/dashboard/uretim/order-form.jsx
@@ -9,7 +9,7 @@ import { SubmitButton, TextInput } from '@/components/common/form-fields';
 import { useRouter } from 'next/navigation';
 import PageHeader from '@/components/common/page-header';
 
-const orderTypes = ['LIFT', 'DAMPER', 'BLOKLIFT', 'PASLANMAZ'];
+const orderTypes = ['Lift', 'Damper', 'Blok Lift', 'Paslanmaz'];
 
 const OrderForm = () => {
     const [state, setState] = useState(initialResponse);
@@ -84,14 +84,14 @@ const OrderForm = () => {
 
         if (response.ok) {
             swAlert(response.message, 'success');
-            // Clear localStorage after successful form submission
-            localStorage.removeItem('orderFormData');
+            
+            localStorage.removeItem('orderFormData');// localStorage'daki form verisini temizler
             router.push('/dashboard/uretim');
         } else if (response.message) {
             swAlert(response.message, 'error');
             setTimeout(() => {
-                window.location.reload(); // Reload the page after showing the error
-            }, 5000); // Slight delay for the error message to be visible
+                window.location.reload(); // Hata mesajini gösterdikten sonra sayfayi yeniler
+            }, 5000); // Hata mesajını belli bir süre görünür olmaisini sağlar
         }
     };
 
