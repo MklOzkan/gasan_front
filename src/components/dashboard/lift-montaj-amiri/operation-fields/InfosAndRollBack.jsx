c37481723fbb8daff7903e4ad9c2e1aa851d7d90
Mikail Ozkan <ozkan.mkl.ozkanQgmail.com>
2024-11-26 02:13:16 +0100

    changes

--- src/components/dashboard/lift-montaj-amiri/operation-fields/InfosAndRollBack.jsx
+++ src/components/dashboard/lift-montaj-amiri/operation-fields/InfosAndRollBack.jsx
@@ -5,6 +5,7 @@ import styles from './infos-and-rollback.module.scss';
 import { Button } from 'react-bootstrap';
 import { swAlert, swConfirm } from '@/helpers/swal';
 import { rollBackLastChangeAction } from '@/actions/lift_actions';
+import { config } from '@/helpers/config';
 
 const OperationsInfo = ({ operations, order }) => {
 
@@ -40,7 +41,13 @@ const OperationsInfo = ({ operations, order }) => {
                     {operations.map((operation, index) => (
                         <React.Fragment key={index}>
                             <tr className={styles.table_body}>
-                                <td>{operation.operationType}</td>
+                                <td>
+                                    {
+                                        config.talasliList[
+                                            operation.operationType
+                                        ]
+                                    }
+                                </td>
                                 <td>
                                     <Button
                                         className={styles.edit_button}
